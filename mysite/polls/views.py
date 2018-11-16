848469417c37f64cc0bb54b29e73aa16c9139f56
kyoko020208 <kyoko020208@gmail.com>
2018-11-15 23:01:15 -0800

    templates  -detail.html  -index.html  -results.html views.py tess.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -14,9 +15,12 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
+    def get_queryset(self):
+        return
+    Question.objects.filter(pub_date__lte=timezone.now())
     model = Question
     template_name = 'polls/detail.html'
 
